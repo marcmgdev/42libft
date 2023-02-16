9a66e1385bbcbed7be64e5574b3c6008a004d77b
Marc Miro Gomez <mmiro-go@car12s1.42barcelona.com>
2023-02-16 16:15:08 +0100

    finally last commit, remake itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,60 +6,51 @@
 /*   By: mmiro-go <mmiro-go@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 12:13:02 by mmiro-go          #+#    #+#             */
-/*   Updated: 2023/02/16 14:45:58 by mmiro-go         ###   ########.fr       */
+/*   Updated: 2023/02/16 16:07:12 by mmiro-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_nbr(long long n, int len, char *r)
+static int	numlen(int n)
 {
-	while (n)
-	{
-		r[len--] = 48 + (n % 10);
-		n /= 10;
-	}
-}
+	int	i;
 
-int	ft_len(int n)
-{
-	int	c;
-
-	c = 0;
-	if (n < 0)
-		c++;
-	while (n)
+	i = 0;
+	if (n == 0)
+		return (1);
+	n = -n;
+	while (n != 0)
 	{
 		n /= 10;
-		c++;
+		i++;
 	}
-	return (c);
+	return (i);
 }
 
 char	*ft_itoa(int n)
 {
-	long long	nb;
-	int			len;
-	char		*r;
+	int		i;
+	int		len;
+	char	*str;
 
-	len = ft_len(n);
-	if (!len)
-	{
-		r = (char *)malloc(2);
-		r[0] = '0';
-		r[1] = 0;
-		return (r);
-	}
-	r = (char *)malloc(len * sizeof(char) + 1);
-	if (!r)
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	i = 0;
+	if (n < 0)
+		i = 1;
+	len = numlen(n) + i;
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
 		return (NULL);
-	r[len--] = 0;
-	nb = n;
-	if (nb < 0)
+	if (n < 0)
+		str[0] = '-';
+	str[len] = '\0';
+	n = n * (((n < 0) * -2) + 1);
+	while (len-- != i)
 	{
-		nb *= -1;
-		r[0] = '-';
+		str[len] = (n % 10) + '0';
+		n /= 10;
 	}
-	ft_nbr(nb, len, r);
-	return (r);
-}	
+	return (str);
+}
